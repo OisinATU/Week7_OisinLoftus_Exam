0cdb302772ae9c3157d2b7a7037d594ee5c0c467
OisinLoftus-STUDENT <G00470518@atu.ie>
2025-11-05 17:15:25 +0000

    New Order operation created

--- src/main/java/org/example/OrderAppClass.java
+++ src/main/java/org/example/OrderAppClass.java
@@ -1,12 +1,88 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
 public class OrderAppClass {
 
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+
+        ArrayList<Orders> orders = new ArrayList<Orders>();
+
+        while (true) {
+
+            System.out.println("Order Management System\n");
+            System.out.println("1) Add Order\n");
+            System.out.println("2) Update Order\n");
+            System.out.println("3) List Orders\n");
+            System.out.println("4) Exit\n\n");
+
+            System.out.println("Please Select Operation:\n");
+
+            int operation = sc.nextInt();
+            sc.nextLine();
+
+            boolean active = true;
+
+            switch(operation){
+                case 1:
+
+                    System.out.println("---Adding an Order---\n");
+                    System.out.println("What is the OrderID:\n");
+                    String orderId = sc.nextLine();
+
+
+
+                    System.out.println("Customer name:\n");
+                    String customerName = sc.nextLine();
+
+                    System.out.println("Product name:\n");
+                    String productName = sc.nextLine();
+
+                    System.out.println("Price:\n");
+                    double price = sc.nextDouble();
+                    sc.nextLine();
+
+                    System.out.println("Quantity:\n");
+                    int quantity = sc.nextInt();
+                    sc.nextLine();
+
+                    System.out.println("Total Price:\n");
+                    double totalPrice = sc.nextDouble();
+                    sc.nextLine();
+
+                    Orders newOrder = new Orders(orderId, customerName, productName, price, quantity, totalPrice);
+                    orders.add(newOrder);
+                    break;
+                case 2:
+
+             
+                        }
+                    }
+
+
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    active = false;
+                    break;
+                default:
+                    System.out.println("Error");
+                    break;
+            }
+
+
+            if(!active){
+                break;
+            }
 
 
 
 
+        }
     }
 }
---
+++ src/main/java/org/example/Orders.java
@@ -0,0 +1,80 @@
+package org.example;
+
+public class Orders {
+    public String orderId;
+    public String customerName;
+    public String productName;
+    public double price;
+    public int quantity;
+    public double totalPrice;
+
+    public Orders(String orderId, String customerName, String productName, double price, int quantity, double totalPrice) {
+        this.orderId = orderId;
+        this.customerName = customerName;
+        this.productName = productName;
+        this.price = price;
+        this.quantity = quantity;
+        this.totalPrice = totalPrice;
+    }
+
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "orderId='" + orderId + '\'' +
+                ", customerName='" + customerName + '\'' +
+                ", productName='" + productName + '\'' +
+                ", price=" + price +
+                ", quantity=" + quantity +
+                ", totalPrice=" + totalPrice +
+                '}';
+    }
+}
